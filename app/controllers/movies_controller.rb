5104b6e3c132212fad1f27a1345fba8a1425e252
Erik Ahumada <costerik@hotmail.com>
2018-10-21 15:55:45 -0500

    created route to test dotenv

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,4 +3,9 @@ class MoviesController < ApplicationController
     all = Movie.all
     render json: all, status: :ok
   end
+
+  def env
+    env = ENV['API_KEY']
+    render json: env, status: :ok
+  end
 end
